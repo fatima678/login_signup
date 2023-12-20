7271295a8763acd03017b37e4e897523bce12e19
fatima678 <119948111+fatima678@users.noreply.github.com>
2023-12-20 23:14:42 +0500

    login signup page with react js

---
+++ src/components/loginsignup/LoginSignup.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './LoginSignup.css'
+// import LoginSignup from './loginsignup'
+
+import email_icon from '../Assets/email.png'
+import password_icon from '../Assets/password.png'
+import person_icon from '../Assets/person.png' 
+
+const LoginSignup = () => {
+  return (
+    <div className='container'>
+        <div className='header'>
+            <div className="text">Signup</div>
+           <div className='underline'></div>
+        </div>
+        <div className='inputs'>
+            <div className='input'>
+                <img className='img' src={person_icon} alt=''></img>
+                <input type='text'/>
+            </div>
+            <div className='input'>
+                <img  className='img'src={email_icon} alt='' ></img>
+                <input type='email'/>
+            </div>
+            <div className='input'>
+                <img className='img'src={password_icon} alt=''></img>
+                <input type='password'/>
+            </div>
+
+        </div>
+        <div className='forgot-password'>Lost password? <span>Click Here!</span></div>
+        <div className='submit-container'>
+            <div className='submit'>Signup</div>
+            <div className='submit'>Login</div>
+        </div>
+    </div>
+  )
+}
+
+export default LoginSignup
